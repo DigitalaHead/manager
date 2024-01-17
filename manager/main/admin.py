fedfec7b20d0e598785da8ee87b35752dfd47ce8
DigitalaHead <magistrskorpion666@gmail.com>
2024-01-18 01:25:59 +0500

    add crud operations in template

--- manager/main/admin.py
+++ manager/main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 admin.site.register(CustomNavigation)
 admin.site.register(CustomMainPage)
 admin.site.register(CustomRelevance)
 admin.site.register(CustomLocation)
 admin.site.register(CustomAbilities)
-admin.site.register(CustomHeadHunterLV)
