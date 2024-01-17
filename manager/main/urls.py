3553b1d8c00b419bdc109e3f083ef9fbfa8fd101
DigitalaHead <magistrskorpion666@gmail.com>
2024-01-17 15:40:31 +0500

    Update urls.py

--- manager/main/urls.py
+++ manager/main/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', render_home, name='index'),
-    path('relevance', demand_page, name='relevance'),
-    path('geography', geography_page, name='geo'),
-    path('abilities', skills_page, name='abilities'),
-    path('api_last_vacancy', last_vacancy_page, name='alv'),
+    path('relevance', render_demand, name='relevance'),
+    path('geography', render_geography, name='geo'),
+    path('abilities', render_skills, name='abilities'),
+    path('api_last_vacancy', render_last_vacancy, name='alv'),
 ]
